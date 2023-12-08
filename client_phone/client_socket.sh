a1c3a6707b8b528367842c9194e5e5b7749873e8
mollyy0514 <molly900514@gmail.com>
2023-12-08 18:26:45 +0800

    try client_socket.sh

--- client_phone/client_socket.sh
+++ client_phone/client_socket.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/data/data/com.termux/files/usr/bin
 export GOMODCACHE=/data/go/pkg/mod
 
 cd /data/data/com.termux/files/home/GO_QUIC_socket
-chmod +x ./socket/client_socket_phone.go
+chmod +x ./socket/client_socket_phone.go 2> output.txt
 # println("Hello World!")
-go run ./socket/client_socket_phone.go > output.txt
+go run ./socket/client_socket_phone.go > output.txt 2>&1 
 # ./socket/client_socket_phone
