fb7391c0603d238acf2bcadce8b5c65a3454093f
mollyy0514 <molly900514@gmail.com>
2023-12-08 18:37:03 +0800

    shell script check

--- client_phone/client_socket.sh
+++ client_phone/client_socket.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/data/data/com.termux/files/usr/bin
 export GOMODCACHE=/data/go/pkg/mod
 
 cd /data/data/com.termux/files/home/GO_QUIC_socket
-chmod +x ./socket/client_socket_phone.go 2> output.txt
+chmod +x ./socket/client_socket_phone.go
 # println("Hello World!")
-go run ./socket/client_socket_phone.go > output.txt 2>&1 
+go run ./socket/client_socket_phone.go
 # ./socket/client_socket_phone
