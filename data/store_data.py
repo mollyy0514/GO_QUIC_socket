9f66c75f0276fb14b151919c765dc18e635c7a78
mollyy0514 <91655983+mollyy0514@users.noreply.github.com>
2024-01-05 12:23:15 +0800

    organize data

--- data/store_data.py
+++ data/store_data.py
@@ -17,12 +17,12 @@ from datetime import date
 def make_folder(date):
     Path(f"./data/{date}").mkdir(parents=True, exist_ok=True)
 
-def move_file(prev, date, s, c, f, k):
+def move_file(prev, date, s, c, k):
     # s: serverside data, c: client side date, f: the time data in payload, k: tls_ley.log
     shutil.move(prev+s, prev+date+"/"+s)
     shutil.move(prev+c, prev+date+"/"+c)
-    shutil.move(prev+f, prev+date+"/"+f)
-    os.rename(prev+date+"/"+f, prev+date+f"/timefloats_{date}.json")
+    # shutil.move(prev+f, prev+date+"/"+f)
+    # os.rename(prev+date+"/"+f, prev+date+f"/timefloats_{date}.json")
     shutil.move(k, prev+date+"/"+k)
 
 date = sys.argv[1]
@@ -31,5 +31,5 @@ make_folder(date)
 date = date[:8]
 s = f"capturequic_s_{date}.pcap"
 c = f"capturequic_c_{date}.pcap"
-f = "timefloats.json"
-move_file("./data/", date, s, c, f, "tls_key.log")
+# f = "timefloats.json"
+move_file("./data/", date, s, c, "tls_key.log")
