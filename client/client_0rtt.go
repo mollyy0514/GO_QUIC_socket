e2d25e932f4087a86c6546bde3a531b22aa704a8
mollyy0514 <91655983+mollyy0514@users.noreply.github.com>
2024-01-07 15:52:22 +0800

    0rtt solvedgit add .git add . & analyze rtt under different network

--- client/client_0rtt.go
+++ client/client_0rtt.go
@@ -1,204 +1,211 @@
 package main
 
-// import (
-// 	"context"
-// 	"crypto/rand"
-// 	"crypto/tls"
-// 	"crypto/x509"
-// 	"encoding/binary"
-// 	"fmt"
+import (
+	"context"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
 
-// 	// "io"
-// 	"log"
-// 	"os"
-// 	"time"
+	// "crypto/x509"
+	"encoding/binary"
+	"fmt"
 
-// 	"github.com/quic-go/quic-go"
-// 	"github.com/quic-go/quic-go/logging"
-// 	"github.com/quic-go/quic-go/qlog"
-// )
+	// "io"
+	"log"
+	// "os"
+	"time"
 
-// const SERVER = "127.0.0.1"
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/logging"
+	"github.com/quic-go/quic-go/qlog"
+)
 
-// // const SERVER = "192.168.1.79" // MacBook Pro M1 local IP
-// // const SERVER = "192.168.1.78" // wmnlab local IP
-// // const SERVER = "140.112.20.183"  // 249 public IP
-// const PORT = 4242
+const SERVER = "127.0.0.1"
 
-// var serverAddr string = fmt.Sprintf("%s:%d", SERVER, PORT)
+// const SERVER = "192.168.1.79" // MacBook Pro M1 local IP
+// const SERVER = "192.168.1.78" // wmnlab local IP
+// const SERVER = "140.112.20.183"  // 249 public IP
+const PORT = 4242
 
-// const PACKET_LEN = 250
+var serverAddr string = fmt.Sprintf("%s:%d", SERVER, PORT)
 
-// func main() {
+const PACKET_LEN = 250
 
-// 	pool, err := x509.SystemCertPool()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	// println("pool", pool)
+func main() {
+	// set the password for sudo
+	// Retrieve command-line arguments
+	args := os.Args
+	// Access the argument at index 1 (index 0 is the program name)
+	password := args[1]
 
-// 	// set TLS
-// 	tlsConfig := &tls.Config{
-// 		InsecureSkipVerify: false,
-// 		RootCAs: pool,
-// 		// KeyLogWriter: keyLog,
-// 		NextProtos:         []string{"h3"},
-// 		ClientSessionCache: tls.NewLRUClientSessionCache(100),
-// 	}
-// 	// var put <-chan struct{}
-// 	// tlsConfig.ClientSessionCache, put = newSessionCache(tls.NewLRUClientSessionCache(1))
-// 	// puts := make(chan string, 100)
-// 	// cache := tlsConfig.ClientSessionCache
-// 	// if cache == nil {
-// 	// 	cache = tls.NewLRUClientSessionCache(100)
-// 	// }
-// 	// tlsConfig.ClientSessionCache = newClientSessionCache(cache, make(chan string, 100), puts)
+	// capture packets in client side
+	subProcess := Start_client_tcpdump(password)
 
-// 	// add 0rtt to quicConfig
-// 	quicConfig := quic.Config{
-// 		Allow0RTT: true,
-// 		Tracer: func(ctx context.Context, p logging.Perspective, connID quic.ConnectionID) *logging.ConnectionTracer {
-// 			role := "server"
-// 			if p == logging.PerspectiveClient {
-// 				role = "client"
-// 			}
-// 			filename := fmt.Sprintf("./log_%s_%s.qlog", connID, role)
-// 			f, err := os.Create(filename)
-// 			if err != nil {
-// 				fmt.Println("cannot generate qlog file")
-// 			}
-// 			// handle the error
-// 			return qlog.NewConnectionTracer(f, p, connID)
-// 		},
-// 		TokenStore: quic.NewLRUTokenStore(100, 100),
-// 	}
+	// pool, err := x509.SystemCertPool()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// println("pool", pool)
+	pool := x509.NewCertPool()
+	AddRootCA(pool)
 
-// 	for i := 0; i < 3; i++ {
-// 		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second) // 3s handshake timeout
-// 		defer cancel()
+	// set TLS
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: false,
+		RootCAs:            pool,
+		// KeyLogWriter: keyLog,
+		NextProtos:         []string{"h3"},
+		ClientSessionCache: tls.NewLRUClientSessionCache(100),
+	}
+	// var put <-chan struct{}
+	// tlsConfig.ClientSessionCache, put = newSessionCache(tls.NewLRUClientSessionCache(1))
+	// puts := make(chan string, 100)
+	// cache := tlsConfig.ClientSessionCache
+	// if cache == nil {
+	// 	cache = tls.NewLRUClientSessionCache(100)
+	// }
+	// tlsConfig.ClientSessionCache = newClientSessionCache(cache, make(chan string, 100), puts)
 
-// 		// connect to server IP. Session is like the socket of TCP/IP
-// 		session, err := quic.DialAddrEarly(ctx, serverAddr, tlsConfig, &quicConfig)
-// 		if err != nil {
-// 			fmt.Println("err: ", err)
-// 		}
-// 		// defer session.CloseWithError(quic.ApplicationErrorCode(501), "hi you have an error")
+	// add 0rtt to quicConfig
+	quicConfig := quic.Config{
+		Allow0RTT: true,
+		Tracer: func(ctx context.Context, p logging.Perspective, connID quic.ConnectionID) *logging.ConnectionTracer {
+			role := "server"
+			if p == logging.PerspectiveClient {
+				role = "client"
+			}
+			filename := fmt.Sprintf("./log_%s_%s.qlog", connID, role)
+			f, err := os.Create(filename)
+			if err != nil {
+				fmt.Println("cannot generate qlog file")
+			}
+			// handle the error
+			return qlog.NewConnectionTracer(f, p, connID)
+		},
+		TokenStore: quic.NewLRUTokenStore(100, 100),
+	}
 
-// 		// create a stream
-// 		// context.Background() is similar to a channel, giving QUIC a way to communicate
-// 		stream, err := session.OpenStreamSync(context.Background())
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		// defer stream.Close()
+	for i := 0; i < 3; i++ {
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second) // 3s handshake timeout
+		defer cancel()
 
-// 		// select {
-// 		// 	case <-time.NewTimer(10 * time.Second).C:
-// 		// 		log.Printf("expected to receive a session ticket within 10 seconds")
-// 		// 	case <-put:
-// 		// 		log.Println("GET session ticket ", put)
-// 		// }
+		// connect to server IP. Session is like the socket of TCP/IP
+		session, err := quic.DialAddrEarly(ctx, serverAddr, tlsConfig, &quicConfig)
+		if err != nil {
+			fmt.Println("err: ", err)
+		}
+		// defer session.CloseWithError(quic.ApplicationErrorCode(501), "hi you have an error")
 
-// 		// Duration to run the sending process
-// 		duration := 5 * time.Second
-// 		seq := 1
-// 		start_time := time.Now()
-// 		euler := 271828
-// 		pi := 31415926
-// 		for time.Since(start_time) <= time.Duration(duration) {
+		// create a stream
+		// context.Background() is similar to a channel, giving QUIC a way to communicate
+		stream, err := session.OpenStreamSync(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+		// defer stream.Close()
 
-// 			t := time.Now().UnixNano() // Time in milliseconds
-// 			print(t, "\n")
-// 			datetimedec := uint32(t / 1e9) // Extract seconds from milliseconds
-// 			microsec := uint32(t % 1e9)    // Extract remaining microseconds
+		// select {
+		// 	case <-time.NewTimer(10 * time.Second).C:
+		// 		log.Printf("expected to receive a session ticket within 10 seconds")
+		// 	case <-put:
+		// 		log.Println("GET session ticket ", put)
+		// }
 
-// 			// var message []byte
-// 			message := Create_packet(uint32(euler), uint32(pi), datetimedec, microsec, uint32(seq))
-// 			Transmit(stream, message)
-// 			time.Sleep(500 * time.Millisecond)
-// 			seq++
-// 		}
-// 		print("times up \n")
+		// Duration to run the sending process
+		duration := 5 * time.Second
+		seq := 1
+		start_time := time.Now()
+		euler := 271828
+		pi := 31415926
+		for time.Since(start_time) <= time.Duration(duration) {
 
-// 		state := session.ConnectionState()
-// 		fmt.Printf("[4242 same]i:%d, used0RTT:%v\n", i, state.Used0RTT)
+			t := time.Now().UnixNano() // Time in milliseconds
+			print(t, "\n")
+			datetimedec := uint32(t / 1e9) // Extract seconds from milliseconds
+			microsec := uint32(t % 1e9)    // Extract remaining microseconds
 
-// 		stream.Close()
-// 		session.CloseWithError(0, "there is an error!")
+			// var message []byte
+			message := Create_packet(uint32(euler), uint32(pi), datetimedec, microsec, uint32(seq))
+			Transmit(stream, message)
+			time.Sleep(500 * time.Millisecond)
+			seq++
+		}
+		print("times up \n")
 
-// 	}
+		state := session.ConnectionState()
+		fmt.Printf("[4242 same]i:%d, used0RTT:%v\n", i, state.Used0RTT)
 
-// }
+		stream.Close()
+		session.CloseWithError(0, "there is an error!")
 
-// func Create_packet(euler uint32, pi uint32, datetimedec uint32, microsec uint32, seq uint32) []byte {
-// 	var message []byte
-// 	message = append(message, make([]byte, 4)...)
-// 	binary.BigEndian.PutUint32(message[:4], euler)
-// 	message = append(message, make([]byte, 4)...)
-// 	binary.BigEndian.PutUint32(message[4:8], pi)
-// 	message = append(message, make([]byte, 4)...)
-// 	binary.BigEndian.PutUint32(message[8:12], datetimedec)
-// 	message = append(message, make([]byte, 4)...)
-// 	binary.BigEndian.PutUint32(message[12:16], microsec)
-// 	message = append(message, make([]byte, 4)...)
-// 	binary.BigEndian.PutUint32(message[16:20], seq)
+	}
+	Close_client_tcpdump(subProcess)
+}
 
-// 	// add random additional data to 250 bytes
-// 	msgLength := len(message)
-// 	if msgLength < PACKET_LEN {
-// 		randomBytes := make([]byte, PACKET_LEN-msgLength)
-// 		rand.Read(randomBytes)
-// 		message = append(message, randomBytes...)
-// 	}
+func Create_packet(euler uint32, pi uint32, datetimedec uint32, microsec uint32, seq uint32) []byte {
+	var message []byte
+	message = append(message, make([]byte, 4)...)
+	binary.BigEndian.PutUint32(message[:4], euler)
+	message = append(message, make([]byte, 4)...)
+	binary.BigEndian.PutUint32(message[4:8], pi)
+	message = append(message, make([]byte, 4)...)
+	binary.BigEndian.PutUint32(message[8:12], datetimedec)
+	message = append(message, make([]byte, 4)...)
+	binary.BigEndian.PutUint32(message[12:16], microsec)
+	message = append(message, make([]byte, 4)...)
+	binary.BigEndian.PutUint32(message[16:20], seq)
 
-// 	return message
-// }
+	// add random additional data to 250 bytes
+	msgLength := len(message)
+	if msgLength < PACKET_LEN {
+		randomBytes := make([]byte, PACKET_LEN-msgLength)
+		rand.Read(randomBytes)
+		message = append(message, randomBytes...)
+	}
 
-// func Transmit(stream quic.Stream, message []byte) {
-// 	_, err := stream.Write(message)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
+	return message
+}
 
-// // type sessionCache struct {
-// // 	tls.ClientSessionCache
-// // 	put chan<- struct{}
-// // }
-// // func newSessionCache(c tls.ClientSessionCache) (tls.ClientSessionCache, <-chan struct{}) {
-// // 	put := make(chan struct{}, 100)
-// // 	return &sessionCache{ClientSessionCache: c, put: put}, put
-// // }
+func Transmit(stream quic.Stream, message []byte) {
+	_, err := stream.Write(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-// // func (c *sessionCache) Put(key string, cs *tls.ClientSessionState) {
-// // 	c.ClientSessionCache.Put(key, cs)
-// // 	c.put <- struct{}{}
-// // }
+func AddRootCA(certPool *x509.CertPool) {
+	caCertRaw, err := os.ReadFile("cert.crt")
+	if err != nil {
+		panic(err)
+	}
+	if ok := certPool.AppendCertsFromPEM(caCertRaw); !ok {
+		panic("Could not add root ceritificate to pool.")
+	}
+}
 
-// // type clientSessionCache struct {
-// // 	cache tls.ClientSessionCache
+func Start_client_tcpdump(password string) *exec.Cmd {
+	currentTime := time.Now()
+	y := currentTime.Year()
+	m := currentTime.Month()
+	d := currentTime.Day()
+	filepath := fmt.Sprintf("../data/capturequic_c_%02d%02d%02d.pcap", y, m, d)
+	command := fmt.Sprintf("echo %s | sudo -S tcpdump port %d -w %s", password, PORT, filepath)
+	subProcess := exec.Command("sh", "-c", command)
 
-// // 	gets chan<- string
-// // 	puts chan<- string
-// // }
+	err := subProcess.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// // func newClientSessionCache(cache tls.ClientSessionCache, gets, puts chan<- string) *clientSessionCache {
-// // 	return &clientSessionCache{
-// // 		cache: cache,
-// // 		gets:  gets,
-// // 		puts:  puts,
-// // 	}
-// // }
+	return subProcess
+}
 
-// // var _ tls.ClientSessionCache = &clientSessionCache{}
-
-// // func (c *clientSessionCache) Get(sessionKey string) (*tls.ClientSessionState, bool) {
-// // 	session, ok := c.cache.Get(sessionKey)
-// // 	c.gets <- sessionKey
-// // 	return session, ok
-// // }
-
-// // func (c *clientSessionCache) Put(sessionKey string, cs *tls.ClientSessionState) {
-// // 	c.cache.Put(sessionKey, cs)
-// // 	c.puts <- sessionKey
-// // }
+func Close_client_tcpdump(cmd *exec.Cmd) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+}
--- server/server_stream.go
+++ server/server_stream.go
@@ -1,215 +1,215 @@
 package main
 
-import (
-	"context"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/tls"
-	"crypto/x509"
-	"encoding/binary"
+// import (
+// 	"context"
+// 	"crypto/rand"
+// 	"crypto/rsa"
+// 	"crypto/tls"
+// 	"crypto/x509"
+// 	"encoding/binary"
 
-	// "encoding/json"
-	"encoding/pem"
-	"os"
-	"time"
+// 	// "encoding/json"
+// 	"encoding/pem"
+// 	"os"
+// 	"time"
 
-	// "errors"
-	"flag"
-	"fmt"
-	"log"
-	"math/big"
-	"os/exec"
+// 	// "errors"
+// 	"flag"
+// 	"fmt"
+// 	"log"
+// 	"math/big"
+// 	"os/exec"
 
-	// "strconv"
-	"github.com/quic-go/quic-go"
-	"github.com/quic-go/quic-go/logging"
-	"github.com/quic-go/quic-go/qlog"
-)
+// 	// "strconv"
+// 	"github.com/quic-go/quic-go"
+// 	"github.com/quic-go/quic-go/logging"
+// 	"github.com/quic-go/quic-go/qlog"
+// )
 
-// const bufferMaxSize = 1048576 // 1mb
-const packet_length = 250
-const SERVER = "0.0.0.0"
-const PORT = 4242
+// // const bufferMaxSize = 1048576 // 1mb
+// const packet_length = 250
+// const SERVER = "0.0.0.0"
+// const PORT = 4242
 
-// We start a server echoing data on the first stream the client opens,
-// then connect with a client, send the message, and wait for its receipt.
-func main() {
-	fmt.Println("Starting server...")
+// // We start a server echoing data on the first stream the client opens,
+// // then connect with a client, send the message, and wait for its receipt.
+// func main() {
+// 	fmt.Println("Starting server...")
 
-	host := flag.String("host", SERVER, "Host to bind")
-	quicPort := flag.Int("quic", PORT, "QUIC port to listen")
-	flag.Parse()
+// 	host := flag.String("host", SERVER, "Host to bind")
+// 	quicPort := flag.Int("quic", PORT, "QUIC port to listen")
+// 	flag.Parse()
 
-	Start_server_tcpdump()
+// 	Start_server_tcpdump()
 
-	go EchoQuicServer(*host, *quicPort)
-	select {}
-}
-
-func HandleQuicStream(stream quic.Stream) {
-
-	seq := 0
-	// Open or create a file to store the floats in JSON format
-	// timeFile, err := os.OpenFile("./data/timefloats.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	// if err != nil {
-	// 	fmt.Println("Error opening file:", err)
-	// 	return
-	// }
-	// defer timeFile.Close()
-	for {
-		buf := make([]byte, packet_length)
-		ts, err := Server_receive(stream, buf)
-		if err != nil {
-			return
-		}
-		fmt.Printf("Received: %f\n", ts)
-
-		// Marshal the float to JSON and append it to the file
-		// encoder := json.NewEncoder(timeFile)
-		// if err := encoder.Encode(ts); err != nil {
-		// 	fmt.Println("Error encoding JSON:", err)
-		// 	return
-		// }
-
-		// sending response to client
-		// responseString := "server received!"
-		// responseMsg := []byte(responseString)
-		// response(stream, responseMsg)
-
-		seq += 1
-	}
-}
-
-func HandleQuicSession(sess quic.Connection) {
-	for {
-		// create a stream to receive message, and also create a channel for communication
-		stream, err := sess.AcceptStream(context.Background())
-		if err != nil {
-			return // Using panic here will terminate the program if a new connection has not come in in a while, such as transmitting large file.
-		}
-		// print remote address & 0rtt
-		state := sess.ConnectionState()
-		log.Printf("Accepted QUIC connection from %v, 0rtt = %v, handshake = %v, TLSresume = %v, proto = %s\n",
-			sess.RemoteAddr(),
-			state.Used0RTT,
-			state.TLS.HandshakeComplete,
-			state.TLS.DidResume,
-			state.TLS.NegotiatedProtocol)
-
-		go HandleQuicStream(stream)
-	}
-}
-
-// Start a server that echos all data on top of QUIC
-func EchoQuicServer(host string, quicPort int) error {
-	quicConfig := quic.Config{
-		KeepAlivePeriod: time.Minute * 5,
-		EnableDatagrams: true,
-		Allow0RTT: true,
-		Tracer: func(ctx context.Context, p logging.Perspective, connID quic.ConnectionID) *logging.ConnectionTracer {
-			role := "server"
-			if p == logging.PerspectiveClient {
-				role = "client"
-			}
-			filename := fmt.Sprintf("./log_%s_%s.qlog", connID, role)
-			f, err := os.Create(filename)
-			if err != nil {
-				fmt.Println("cannot generate qlog file")
-			}
-			// handle the error
-			return qlog.NewConnectionTracer(f, p, connID)
-		},
-	}
-	// ListenAddrEarly supports 0rtt
-	listener, err := quic.ListenAddr(fmt.Sprintf("%s:%d", host, quicPort), GenerateTLSConfig(), &quicConfig)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Started QUIC server! %s:%d\n", host, quicPort)
-
-	for {
-		// create a session
-		sess, err := listener.Accept(context.Background())
-		fmt.Printf("Accepted Connection! %s\n", sess.RemoteAddr())
-
-		if err != nil {
-			return err
-		}
-
-		go HandleQuicSession(sess)
-	}
-}
-
-// Setup a bare-bones TLS config for the server
-func GenerateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		panic(err)
-	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	kl, _ := os.OpenFile("tls_key.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		NextProtos:   []string{"h3"},
-		KeyLogWriter: kl,
-	}
-}
-
-// func pad(bb []byte, size int) []byte {
-// 	l := len(bb)
-// 	if l == size {
-// 		return bb
-// 	}
-// 	tmp := make([]byte, size)
-// 	copy(tmp[size-l:], bb)
-// 	return tmp
+// 	go EchoQuicServer(*host, *quicPort)
+// 	select {}
 // }
 
-func Start_server_tcpdump() {
-	currentTime := time.Now()
-	y := currentTime.Year()
-	m := currentTime.Month()
-	d := currentTime.Day()
-	date := fmt.Sprintf("%02d%02d%02d", y, m, d)
-	filepath := fmt.Sprintf("./data/capturequic_s_%s.pcap", date)
-	command := fmt.Sprintf("sudo tcpdump port %d -w %s", PORT, filepath)
-	cmd := exec.Command("sh", "-c", command)
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+// func HandleQuicStream(stream quic.Stream) {
 
-func Server_receive(stream quic.Stream, buf []byte) (float64, error) {
-	_, err := stream.Read(buf)
-	tsSeconds := binary.BigEndian.Uint32(buf[8:12])
-	tsMicroseconds := binary.BigEndian.Uint32(buf[12:16])
-	ts := float64(tsSeconds) + float64(tsMicroseconds)/1e10
-	if err != nil {
-		return -115, err
-		// fmt.Println(err)
-	}
+// 	seq := 0
+// 	// Open or create a file to store the floats in JSON format
+// 	// timeFile, err := os.OpenFile("./data/timefloats.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+// 	// if err != nil {
+// 	// 	fmt.Println("Error opening file:", err)
+// 	// 	return
+// 	// }
+// 	// defer timeFile.Close()
+// 	for {
+// 		buf := make([]byte, packet_length)
+// 		ts, err := Server_receive(stream, buf)
+// 		if err != nil {
+// 			return
+// 		}
+// 		fmt.Printf("Received: %f\n", ts)
 
-	return ts, err
-}
+// 		// Marshal the float to JSON and append it to the file
+// 		// encoder := json.NewEncoder(timeFile)
+// 		// if err := encoder.Encode(ts); err != nil {
+// 		// 	fmt.Println("Error encoding JSON:", err)
+// 		// 	return
+// 		// }
 
-// func response(stream quic.Stream, responseMsg []byte) {
-// 	_, err := stream.Write(responseMsg)
+// 		// sending response to client
+// 		// responseString := "server received!"
+// 		// responseMsg := []byte(responseString)
+// 		// response(stream, responseMsg)
+
+// 		seq += 1
+// 	}
+// }
+
+// func HandleQuicSession(sess quic.Connection) {
+// 	for {
+// 		// create a stream to receive message, and also create a channel for communication
+// 		stream, err := sess.AcceptStream(context.Background())
+// 		if err != nil {
+// 			return // Using panic here will terminate the program if a new connection has not come in in a while, such as transmitting large file.
+// 		}
+// 		// print remote address & 0rtt
+// 		state := sess.ConnectionState()
+// 		log.Printf("Accepted QUIC connection from %v, 0rtt = %v, handshake = %v, TLSresume = %v, proto = %s\n",
+// 			sess.RemoteAddr(),
+// 			state.Used0RTT,
+// 			state.TLS.HandshakeComplete,
+// 			state.TLS.DidResume,
+// 			state.TLS.NegotiatedProtocol)
+
+// 		go HandleQuicStream(stream)
+// 	}
+// }
+
+// // Start a server that echos all data on top of QUIC
+// func EchoQuicServer(host string, quicPort int) error {
+// 	quicConfig := quic.Config{
+// 		KeepAlivePeriod: time.Minute * 5,
+// 		EnableDatagrams: true,
+// 		Allow0RTT: true,
+// 		Tracer: func(ctx context.Context, p logging.Perspective, connID quic.ConnectionID) *logging.ConnectionTracer {
+// 			role := "server"
+// 			if p == logging.PerspectiveClient {
+// 				role = "client"
+// 			}
+// 			filename := fmt.Sprintf("./log_%s_%s.qlog", connID, role)
+// 			f, err := os.Create(filename)
+// 			if err != nil {
+// 				fmt.Println("cannot generate qlog file")
+// 			}
+// 			// handle the error
+// 			return qlog.NewConnectionTracer(f, p, connID)
+// 		},
+// 	}
+// 	// ListenAddrEarly supports 0rtt
+// 	listener, err := quic.ListenAddr(fmt.Sprintf("%s:%d", host, quicPort), GenerateTLSConfig(), &quicConfig)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	fmt.Printf("Started QUIC server! %s:%d\n", host, quicPort)
+
+// 	for {
+// 		// create a session
+// 		sess, err := listener.Accept(context.Background())
+// 		fmt.Printf("Accepted Connection! %s\n", sess.RemoteAddr())
+
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		go HandleQuicSession(sess)
+// 	}
+// }
+
+// // Setup a bare-bones TLS config for the server
+// func GenerateTLSConfig() *tls.Config {
+// 	key, err := rsa.GenerateKey(rand.Reader, 1024)
 // 	if err != nil {
 // 		panic(err)
 // 	}
+// 	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+// 	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+// 	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+// 	kl, _ := os.OpenFile("tls_key.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+
+// 	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return &tls.Config{
+// 		Certificates: []tls.Certificate{tlsCert},
+// 		NextProtos:   []string{"h3"},
+// 		KeyLogWriter: kl,
+// 	}
 // }
+
+// // func pad(bb []byte, size int) []byte {
+// // 	l := len(bb)
+// // 	if l == size {
+// // 		return bb
+// // 	}
+// // 	tmp := make([]byte, size)
+// // 	copy(tmp[size-l:], bb)
+// // 	return tmp
+// // }
+
+// func Start_server_tcpdump() {
+// 	currentTime := time.Now()
+// 	y := currentTime.Year()
+// 	m := currentTime.Month()
+// 	d := currentTime.Day()
+// 	date := fmt.Sprintf("%02d%02d%02d", y, m, d)
+// 	filepath := fmt.Sprintf("./data/capturequic_s_%s.pcap", date)
+// 	command := fmt.Sprintf("sudo tcpdump port %d -w %s", PORT, filepath)
+// 	cmd := exec.Command("sh", "-c", command)
+// 	err := cmd.Start()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 // }
+
+// func Server_receive(stream quic.Stream, buf []byte) (float64, error) {
+// 	_, err := stream.Read(buf)
+// 	tsSeconds := binary.BigEndian.Uint32(buf[8:12])
+// 	tsMicroseconds := binary.BigEndian.Uint32(buf[12:16])
+// 	ts := float64(tsSeconds) + float64(tsMicroseconds)/1e10
+// 	if err != nil {
+// 		return -115, err
+// 		// fmt.Println(err)
+// 	}
+
+// 	return ts, err
+// }
+
+// // func response(stream quic.Stream, responseMsg []byte) {
+// // 	_, err := stream.Write(responseMsg)
+// // 	if err != nil {
+// // 		panic(err)
+// // 	}
+// // }
+// // }
