111a8561549878bb3c421fab4ce66814a8d2a632
mollyy0514 <91655983+mollyy0514@users.noreply.github.com>
2024-04-19 15:08:01 +0800

    change phone serial

--- android/setprop_all_mobile.py
+++ android/setprop_all_mobile.py
@@ -11,8 +11,8 @@ import os
 import sys
 
 serial_to_device = {
-    "R5CRA1ET5KB":"sm00",
-    "R5CRA1D2MRJ":"sm01",
+    "R5CR20FDXHK":"sm00",
+    "R5CR30P9Z8Y":"sm01",
     "R5CRA1GCHFV":"sm02",
     "R5CRA1JYYQJ":"sm03",
     "R5CRA1EV0XH":"sm04",
@@ -24,28 +24,13 @@ serial_to_device = {
     "73e11a9f":"xm00",
     "491d5141":"xm01",
     "790fc81d":"xm02",
-    "e2df293a":"xm03",
-    "28636990":"xm04",
-    "f8fe6582":"xm05",
-    "d74749ee":"xm06",
-    "10599c8d":"xm07",
-    "57f67f91":"xm08",
-    "232145e8":"xm09",
-    "70e87dd6":"xm10",
-    "df7aeaf8":"xm11",
-    "e8c1eff5":"xm12",
-    "ec32dc1e":"xm13",
-    "2aad1ac6":"xm14",
-    "64545f94":"xm15",
-    "613a273a":"xm16",
-    "fe3df56f":"xm17",
     "76857c8" :"qc00",
     "bc4587d" :"qc01",
     "5881b62f":"qc02",
     "32b2bdb2":"qc03",
 }
 
-os.system("echo wmnlab | sudo -S su")
+os.system(f"echo {sys.argv[1]} | sudo -S su")
 
 devices_info = []
 for i, info in enumerate(adb.list()):
@@ -74,13 +59,3 @@ for info in adb.list():
 for device, info in zip(devices, devices_info):
     print(info[2], device.shell("su -c 'getprop sys.usb.config'"))
     print(info[2], device.shell("su -c 'setprop sys.usb.config diag,serial_cdev,rmnet,adb'"))
-
-# for device, info in zip(devices, devices_info):
-    # device.shell("su -c 'mount -o remount,rw /system/bin'")
-    # GO environment setting
-#     device.shell("su -c 'export GOCAHE=/data/go-build'")
-#     device.shell("su -c 'PATH=$PATH:/data/data/com.termux/files/usr/bin'")
-#     device.shell("su -c 'export GOMODCAHE=/data/go/pkg/mod'")
-    
-    # cd to GO_QUIC_socket
-    # device.shell("su -c 'cd /data/data/com.termux/files/home/GO_QUIC_socket'")
